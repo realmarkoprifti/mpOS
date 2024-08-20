969f5ad870e058b0c6b1d94403d2ce3c34b4f919
Marko Prifti <marko.prifti4@gmail.com>
2024-08-20 18:21:39 +0200

    Implement testing capability for mpOS

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "testmain"]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 mod vga;
 
@@ -14,5 +25,9 @@ pub fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World");
+
+    #[cfg(test)]
+    testmain();
+
     loop {}
 }
