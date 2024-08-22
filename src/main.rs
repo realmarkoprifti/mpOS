f432073ce826ab159aaf1bc4a7b2a4fe61ecd7d7
Marko Prifti <marko.prifti4@gmail.com>
2024-08-22 14:49:05 +0200

    Added hardware interrupts and fixes to prevent deadlocks

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ pub trait Test {
     fn run(&self) -> ();
 }
 
-impl<T> Test for T where T: Fn(), {
+impl<T> Test for T
+where
+    T: Fn(),
+{
     fn run(&self) {
         sprint!("{}...\t", core::any::type_name::<T>());
         self();
@@ -29,11 +32,11 @@ pub fn test_runner(tests: &[&dyn Test()]) {
     exit_qemu(qemu::QemuExitCode::Success);
 }
 
+mod gdt;
+mod interrupts;
 mod qemu;
 mod serial;
 mod vga;
-mod interrupts;
-mod gdt;
 
 use core::panic::PanicInfo;
 
@@ -41,7 +44,9 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    loop {
+        hlt()
+    }
 }
 
 #[cfg(test)]
@@ -50,31 +55,35 @@ fn panic(info: &PanicInfo) -> ! {
     sprintln!("[failed]\n");
     sprintln!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    loop {
+        hlt()
+    }
 }
 
 // Used for initializing routines
 fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
+}
+
+fn hlt() {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World");
-
     init();
-
-    // doing a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u8) = 42;
-    };
-    
 
     #[cfg(test)]
     testmain();
 
     println!("No crashes!");
 
-    loop {}
+    loop {
+        hlt();
+    }
 }
